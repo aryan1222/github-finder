c96d3a713677ed8a8d026834439c20215a46e590
aryan1222 <chhabraaryan12@gmail.com>
2021-03-31 11:49:12 +0530

    all functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -9,74 +9,69 @@ import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import axios from 'axios';
 
-class App extends Component {
-
-  constructor(props) {
-    super(props)
+const App = () => {
   
-    this.state = {
-       loading : false,
-       user : {},
-       users : [],
-       alert : null
-    }
-  }
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+  const [users, setUsers] = useState([]);
+  const [alert, setAlert] = useState(null);
 
-  searchUser = async text =>{
-    this.setState({loading: true});
+  const searchUser = async text =>{
+    setLoading(true);
+    
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}
                                   &client_secret=${process.env.REACT_APP_CLIENT_SECRET}`);
-    this.setState({loading : false, users : res.data.items});
+    
+    setUsers(res.data.items);
+    setLoading(false);
   }
 
-  getUser = async (username) => {
-    this.setState({loading: true});
+  const getUser = async (username) => {
+    setLoading(true);
+    
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_CLIENT_ID}
                                   &client_secret=${process.env.REACT_APP_CLIENT_SECRET}`);
-    this.setState({loading : false, user : res.data});
+    
+    setUser(res.data);
+    setLoading(false);
+
   }
 
-  clearUsers = () => {
-    this.setState({
-      users: [],
-      loading : false
-    })
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
   }
 
-  setAlert = (msg, type) => {
-    this.setState({alert : {msg : msg, type: type}});
-
-    setTimeout(() => this.setState({alert:null}), 5000);
+  const showAlert = (msg, type) => {
+    setAlert({msg : msg, type: type})
+    
+    setTimeout(() => setAlert(null), 5000);
   }
 
-  render() {
-    const { user, users, loading} = this.state;
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <div className="container">
+        <Alert alert={alert}/>
+        <Switch>
+          <Route exact path='/' render={() => (
+            <Fragment>
+              <Search searchUser={searchUser} clearUsers={clearUsers} 
+                      showClear={users.length > 0 ? true: false} setAlert={showAlert}/>
+              <Users loading={loading} users={users}/>
+            </Fragment>
+          )} />
 
-    return (
-      <BrowserRouter>
-        <Navbar/>
-        <div className="container">
-          <Alert alert={this.state.alert}/>
-          <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search searchUser={this.searchUser} clearUsers={this.clearUsers} 
-                        showClear={users.length > 0 ? true: false} setAlert={this.setAlert}/>
-                <Users loading={loading} users={users}/>
-              </Fragment>
-            )} />
-
-            <Route exact path='/about' component={About} />
-            <Route exact path='/user/:login' render={props => (
-              <User {...props} getUser={this.getUser} user={user} loading={loading}/>
-            )} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/user/:login' render={props => (
+            <User {...props} getUser={getUser} user={user} loading={loading}/>
+          )} />
 
 
-          </Switch>
-        </div>
-      </BrowserRouter>
-    )
-  }
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 
 }
 
